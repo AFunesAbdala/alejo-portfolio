e42433623718642ce6d6dcf6f9019567f5c5a060
Alejo Funes Abdala <alejofunes1@gmail.com>
2026-04-05 16:43:17 -0300

    hover fixed color

--- app/page.js
+++ app/page.js
@@ -127,7 +127,7 @@ export default function Home() {
         </div>
         <a
           href="/adinfluence"
-          className="relative h-45 flex items-center justify-center bg-zinc-100 rounded-xl col-span-2 duration-200 hover:scale-105 hover:ring-1 hover:ring-white"
+          className="relative h-45 flex items-center justify-center bg-zinc-100 rounded-xl col-span-2 duration-400 hover:scale-105 hover:ring-1 hover:ring-white"
           onMouseEnter={() => {
             setIsHoverProyect(true);
           }}
@@ -140,12 +140,12 @@ export default function Home() {
             className="w-full h-full object-cover rounded-xl"
           ></img>
           <div className="absolute flex items-end justify-center h-full w-full pb-4 duration-800 opacity-0 hover:opacity-100 overflow-hidden">
-            <p className="text-center text-white text-sm">
+            <p className="relative z-10 text-center text-white text-sm">
               Adinfluence
               <br></br>
               Website Redesign
             </p>
-            <div className="pointer-events-none absolute -bottom-5/5 w-[200px] h-[200px] rounded-full bg-white blur-2xl"></div>
+            <div className="pointer-events-none absolute -bottom-5/5 w-[200px] h-[200px] rounded-full bg-[#731DFF] blur-2xl"></div>
           </div>
           <HoverCircles number={15} status={isHoverProyect}></HoverCircles>
         </a>
